9603672cf13fb1d89220a609d646b5af089ed812
Christian Decembrana <work.christiandecembrana@gmail.com>
2019-05-14 10:02:25 +0800

    asdadadadad

--- public/js/default.js
+++ public/js/default.js
@@ -1,26 +1,36 @@
+//Animation On Scroll
 AOS.init();
 
 // Menu Action when toggled
-function menuToggle(toggled, overlay, mainNav, iconBar) {   
-    if(toggled == false) {
+function menuToggle(toggled, overlay, mainNav, iconBar) 
+{   
+    if(toggled == false) 
+    {
         $(overlay).css({ "transform" : "scale(80)" });        
-        $(mainNav).css({             
-            "opacity" : "1",
-            "width" : "100%",
-            "margin-left" : "0"
-        });        
+        $(mainNav).css
+        (
+            {             
+                "opacity" : "1",
+                "width" : "100%",
+                "margin-left" : "0"
+            }
+        );        
         $(mainNav).delay(800).css({ "display" : "block" });
         $(iconBar).css({ "background-color":"transparent" });
         $("body").append('<style>'+iconBar+':after{ top: 0; transform: rotate(135deg); }</style>');
         $("body").append('<style>'+iconBar+':before{ top: 0; transform: rotate(-135deg); }</style>');
     }   
-    else {
+    else 
+    {
         $(overlay).css({ "transform" : "scale(1)" });        
-        $(mainNav).css({ 
-            "opacity" : "0",
-            "width" : "0",
-            "margin-left" : "-300px"
-        });              
+        $(mainNav).css
+        (
+            { 
+                "opacity" : "0",
+                "width" : "0",
+                "margin-left" : "-300px"
+            }
+        );              
         $(iconBar).css({ "background-color":"#333" });
         $("body").append('<style>'+iconBar+':after{ top: 15px; transform: rotate(0); }</style>');
         $("body").append('<style>'+iconBar+':before{ top: -15px; transform: rotate(0); }</style>');
@@ -28,20 +38,29 @@ function menuToggle(toggled, overlay, mainNav, iconBar) {
 }
 
 // Initialize menu display
-function menuDisplay() {
+function menuDisplay() 
+{
     var toggled = false,
         overlay = ".overlay",
         mainNav = ".main-nav",
         iconBar = ".nav-toggle-btn .nav-icon-bar";     
   
-    $('.nav-toggle-btn').click(function (e) { 
-        menuToggle(toggled, overlay, mainNav, iconBar);
-        toggled = !toggled;
-    });   
+    $('.nav-toggle-btn').click
+    (
+        function (e) 
+        { 
+            menuToggle(toggled, overlay, mainNav, iconBar);
+            toggled = !toggled;
+        }
+    );   
 
-    $('.nav-links').click(function (e) { 
+    $('.nav-links').click
+    (
+        function (e) 
+        { 
         menuToggle(toggled, overlay, mainNav, iconBar);
         toggled = !toggled;
-    });
+        }
+    );
 
 } menuDisplay();
--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -10,8 +10,62 @@ class BaseController extends AbstractController
     /**
      * @Route("/", name="app_index")
      */
+
     public function index()
     {
-        return $this->render('base/index.html.twig');
+        $skills = array
+                        (
+                            array
+                            (
+                                "title" => "Front-end Developer",
+                                "icon" => "pencil-ruler",
+                                "description" => "CSS3, HTML5, Symfony2, ReactJS, Laravel, Wordpress, Angular, Bootstrap, Jquery, SASS, LESS, MaterializeCSS, Responsiveness, UX, UI"
+                            ),
+                            array
+                            (
+                                "title" => "Back-end Developer",
+                                "icon" => "code",
+                                "description" => "PHP, Javascript, Java, MySQL, NoSQL, MonggoDB, SOLID Principle, NodeJS, ExpressJS, Software Design Patterns, Redux, RxJava, Retrofit, RESTful API, Dagger 2, Butterknife, SOLID Principle, Software Design Patterns"
+                            ),
+                            array
+                            (
+                                "title" => "Tools",
+                                "icon" => "tools",
+                                "description" => "Vscode, Git, Github, Gitlab, JIRA, Terminal, Adobe Photoshop & Illustrator, Filezilla, Android Studio, Eclipse Oxygen, SVN, XAMPP"
+                            ),                            
+                        );
+        $projects = array
+                        (
+                            array
+                            (
+                                "title" => "Project 1",
+                                "description" => "Sample description 1"
+                            ),
+                            array
+                            (
+                                "title" => "Project 2",
+                                "description" => "Sample description 2"
+                            ),
+                            array
+                            (
+                                "title" => "Project 3",
+                                "description" => "Sample description 3"
+                            ),
+                            array
+                            (
+                                "title" => "Project 4",
+                                "description" => "Sample description 4"
+                            ),
+                            array
+                            (
+                                "title" => "Project 5",
+                                "description" => "Sample description 5"
+                            ),
+                        );
+        return $this->render
+                        (
+                            'base/index.html.twig',
+                            ['skills' => $skills, 'projects' => $projects]
+                        );
     }
 }
